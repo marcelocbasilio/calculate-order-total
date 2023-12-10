66549de9561a5c9039d857c033d76715b7d2b549
Marcelo Basílio <marcelocbasilio@gmail.com>
2023-12-09 21:04:41 -0300

    [first-commit]: Setting up the environment.

---
+++ src/main/java/br/com/marcelocbasilio/cot/CotApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcelocbasilio.cot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/marcelocbasilio/cot/CotApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcelocbasilio.cot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
